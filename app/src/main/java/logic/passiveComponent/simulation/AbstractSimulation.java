37e7894b9d3a128c002e1049f1898d588031f905
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-04-02 12:06:23 +0200

    refactor

--- app/src/main/java/logic/passiveComponent/simulation/AbstractSimulation.java
+++ app/src/main/java/logic/passiveComponent/simulation/AbstractSimulation.java
@@ -49,7 +49,7 @@ public abstract class AbstractSimulation implements Simulation {
 		this.environment.step();
 		int effectiveNumOfThread = Math.min(numOfThread, agents.size());
 		this.barrier = new CyclicBarrier(effectiveNumOfThread + 1, this::postProcessing);
-		new MasterWorkerHandler(effectiveNumOfThread,
+		TaskSplitter.splitTasks(effectiveNumOfThread,
 				agents, numSteps, barrier);
 		this.notifyReset(this.t0, this.agents, this.environment);
 	}
